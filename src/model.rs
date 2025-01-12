9ca36736794a7f2b06b4ff60afa79da378c9a4dc
B1uee <379264406@qq.com>
2025-01-13 00:14:40 +0800

    homework

--- src/model.rs
+++ src/model.rs
@@ -3,7 +3,7 @@ use std::vec;
 
 use crate::config::LlamaConfigJson;
 use crate::kvcache::KVCache;
-use crate::operators as OP;
+use crate::operators::{self as OP, matmul_transb, rms_norm, swiglu};
 use crate::params::LLamaParams;
 use crate::tensor::Tensor;
 use safetensors::SafeTensors;
@@ -167,7 +167,12 @@ fn mlp(
     rms_w: &Tensor<f32>,
     eps: f32,
 ) {
-    todo!("Implement mlp");
+    //todo!("Implement mlp");
+    rms_norm(hidden_states, residual, rms_w, eps);
+    matmul_transb(gate, 0., hidden_states, w_gate, 1.);
+    matmul_transb(up, 0., hidden_states, w_up, 1.);
+    swiglu(up, gate);
+    matmul_transb(residual, 1., up, w_down, 1.);
 }
 
 #[test]
--- src/params.rs
+++ src/params.rs
@@ -22,14 +22,60 @@ pub struct LLamaParams<T> {
 
 impl LLamaParams<f32> {
     pub fn from_safetensors(safetensor: &SafeTensors, config: &LlamaConfigJson) -> Self {
-        todo!("实现从safetensors文件的模型参数加载");
-        // let get_tensor: impl Fn(&str) -> Tensor<f32> = |name: &str| {
-        // ...    
-        // };
+        //todo!("实现从safetensors文件的模型参数加载");
+        //let get_tensor: impl Fn(&str) -> Tensor<f32> = |name: &str| { 
+            // impl Trait 只能用于函数和方法的参数和返回类型，不能用于局部变量(get_tensor)的类型声明
         
-        // LLamaParams {
-        //     embedding_table: get_tensor(...),
-        //     ...
-        // }
+        // 此时，闭包的类型会自动推断，不需要使用 impl Trait
+        let get_tensor = |name: &str| -> Tensor<f32> {
+            let tensor = safetensor.tensor(name).unwrap();
+            let shape = tensor.shape();
+            let data = tensor.data();
+            let size: usize = shape.iter().copied().product();  // 获取所有元素的乘积，copied()去掉引用
+            let mut new_data = Vec::new();
+            if data.len() / std::mem::size_of::<f32>() == size {
+                new_data = data
+                    .chunks_exact(std::mem::size_of::<f32>())
+                    .filter_map(|chunk| chunk.try_into().ok().map(f32::from_le_bytes))
+                    .collect();
+            }
+            Tensor::new(new_data, &shape.to_vec())
+
+
+        };
+        
+        LLamaParams {
+            embedding_table: get_tensor("lm_head.weight"),
+            rms_att_w: (0..config.num_hidden_layers)
+                .map(|i| get_tensor(&format!("model.layers.{}.input_layernorm.weight", i)))
+                .collect(),
+            wq: (0..config.num_hidden_layers)
+                .map(|i| get_tensor(&format!("model.layers.{}.self_attn.q_proj.weight", i)))
+                .collect(),
+            wk: (0..config.num_hidden_layers)
+                .map(|i| get_tensor(&format!("model.layers.{}.self_attn.k_proj.weight", i)))
+                .collect(),
+            wv: (0..config.num_hidden_layers)
+                .map(|i| get_tensor(&format!("model.layers.{}.self_attn.v_proj.weight", i)))
+                .collect(),
+            wo: (0..config.num_hidden_layers)
+                .map(|i| get_tensor(&format!("model.layers.{}.self_attn.o_proj.weight", i)))
+                .collect(),
+            rms_ffn_w: (0..config.num_hidden_layers)
+                .map(|i| get_tensor(&format!("model.layers.{}.post_attention_layernorm.weight", i)))
+                .collect(),
+            w_up: (0..config.num_hidden_layers)
+                .map(|i| get_tensor(&format!("model.layers.{}.mlp.up_proj.weight", i)))
+                .collect(),
+            w_gate: (0..config.num_hidden_layers)
+                .map(|i| get_tensor(&format!("model.layers.{}.mlp.gate_proj.weight", i)))
+                .collect(),
+            w_down: (0..config.num_hidden_layers)
+                .map(|i| get_tensor(&format!("model.layers.{}.mlp.down_proj.weight", i)))
+                .collect(),
+            rms_out_w: get_tensor("model.norm.weight"),
+            lm_head: get_tensor("lm_head.weight"),
+            
+        }
     }
 }
